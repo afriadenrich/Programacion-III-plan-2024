640edbd1f6cd65d6a3bcc776df61088274ace0e7
AgustinFrich <agustinfrich@gmail.com>
2024-09-27 20:31:26 -0300

    Clase 10 Asincronismos

--- Clase10/asincronismos.js
+++ Clase10/asincronismos.js
@@ -1,27 +1,61 @@
 // Fetch en modo GET.
 
-const promesa = fetch("https://api.github.com/users/afriadenrich");
+function traerDatosPromesa() {
+  const promesa = fetch("https://api.github.com/users/afriadenrich");
 
-// Asincronismos
-promesa.then((response) => {
-  console.log(response.body);
+  // Asincronismos
+  // Promesa.
+  promesa
+    .then((response) => {
+      if (!response.ok) {
+        throw response.status;
+      }
+      console.log(response.body);
 
-  response.json().then((valor) => {
-    console.log(valor);
+      response.json().then((valor) => {
+        console.log(valor);
 
-    const titulo = document.createElement("h1");
-    titulo.innerText = valor.login;
+        const titulo = document.createElement("h1");
+        titulo.innerText = valor.login;
 
-    document.body.insertAdjacentElement("beforeend", titulo);
+        document.body.insertAdjacentElement("beforeend", titulo);
+      });
+    })
+    .catch((reason) => {
+      console.log(reason);
+    });
+
+  promesa.finally(() => {
+    console.log("se ejecuta después!");
   });
-});
 
-// Promesa.
+  console.log(promesa);
+}
 
 // Async.
 
-console.log(promesa);
+async function traerDatosAsync() {
+  try {
+    const respuesta = await fetch("https://api.github.com/users/afriadenrich");
 
+    const json = await respuesta.json();
+
+    console.log(json);
+
+    const titulo = document.createElement("h1");
+    titulo.innerText = json.login;
+
+    document.body.insertAdjacentElement("beforeend", titulo);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log("Finally!");
+  }
+}
+
+traerDatosAsync();
+
+console.log("Código que está después.");
 //
 //
 //
